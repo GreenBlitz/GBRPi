fd4fe1d8d07eb4a45cbd274cbe2b6e047368118e
asaf8888 <asaf2048@gmail.com>
2019-12-17 19:46:34 +0200

    made i2c stuff generic

--- gbrpi/electronics/i2c.py
+++ gbrpi/electronics/i2c.py
@@ -1,29 +1,49 @@
-from smbus2 import SMBus
-import time
-def check_request(dist, angle):
-    bus = SMBus(1)
-    b = bus.read_byte_data(1, 0)
-    bus.close()
-    if b == 1:
-        send_target(dist, angle)
-    else:
-        return(1)
+from typing import List
 
-def send_target(dist, angle):
-    data = bytearray(dist)
-    data2 = bytearray(angle)
-    data.append(data2)
-    bus = SMBus(1)
-    bus.write_i2c_block_data(2, 0, data)
+from smbus2 import SMBus, i2c_msg
 
-def main():
-    dist = 22.2
-    angle = 187
-    z = 1
-    while True:
-        if check_request(dist,angle) != 1:
-            break
-        z =+ 1
-        time.sleep(0.01)
-        if z == 10000:
-            break
+class I2C:
+    @staticmethod
+    def __prep_data(data: List[int]) -> List[int]:
+        ln = len(data)
+        arr = [0, 0, 0, 0]
+        for i in range(4):
+            arr[3 - i] = ln & 0xFF
+            ln >>= 8
+        return arr + data
+
+    @staticmethod
+    def __get_read_length(bytelength: List[int]) -> int:
+        result = 0
+        for i in range(4):
+            result <<= 8
+            result |= bytelength[i]
+        return result
+
+    def __init__(self, input_addr: int, output_addr: int, bus=1, force=False):
+        self.force = force
+        self.bus = bus
+        self.input_addr = input_addr
+        self.output_addr = output_addr
+
+    def send(self, data: List[int]):
+        """
+
+        """
+        data = self.__prep_data(data)
+        with SMBus(self.bus, force=self.force) as bus:
+            msg = i2c_msg.write(self.output_addr, data)
+            bus.i2c_rdwr(msg)
+
+    def receive(self) -> List[int]:
+        """
+
+        """
+        with SMBus(self.bus, force=self.force) as bus:
+            msg = i2c_msg.read(self.input_addr, 4)
+            bus.i2c_rdwr(msg)
+            length = list(bytes(msg))
+            msg_len = self.__get_read_length(list(bytes(msg)))
+            msg = i2c_msg.read(self.input_addr, msg_len)
+            bus.i2c_rdwr(msg)
+            return list(bytes(msg))
