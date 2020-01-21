d016eaee40d6f27102d213d36552ae3688d3fdb5
idoheinemann <idohaineman@gmail.com>
2020-01-21 16:03:45 +0200

    added I2C to __init__:

--- gbrpi/__init__.py
+++ gbrpi/__init__.py
@@ -4,6 +4,7 @@ from .constants.types import Primitive, ConnEntryValue
 # electronics
 from .electronics.gpio_device import GPIODevice
 from .electronics.led_ring import LedRing
+from .electronics.i2c import I2C
 
 # net
 from .net.table_conn import TableConn
