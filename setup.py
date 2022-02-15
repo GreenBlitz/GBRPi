4757d5a5e87f2e903c384ab872f54ae36e8b7932
Xiddoc <46180972+Xiddoc@users.noreply.github.com>
2022-02-15 17:45:21 +0200

    Updated version :star2:

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='gbrpi',
-    version='0.0.5',
+    version='0.0.6',
     description='A python library for Raspberry Pi operations used for FRC',
     license='Apache License 2.0',
     packages=['gbrpi',
